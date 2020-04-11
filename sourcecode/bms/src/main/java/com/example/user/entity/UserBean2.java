5e075b6e2acbdd277502074e64f7e5365f0f2ef6
xuansonkrt <xuansonkrt@gmail.com>
2020-04-11 22:54:44 +0700

    UserBean2.java

---
+++ sourcecode/bms/src/main/java/com/example/user/entity/UserBean2.java
@@ -0,0 +1,202 @@
+package com.example.user.entity;
+
+
+public class UserBean2 {
+    private Long       userId;
+    private String     userName;
+    private String     password;
+    private String     fullName;
+    private String     dateOfBirth;
+    private Long       gender;
+    private String     email;
+    private String     mobileNumber;
+    private Long       positionId;
+    private Long       roleId;
+    private String     className;
+    private String     majorName;
+    private String     departmentName;
+    private String     userCode;
+    private String     role;
+    private String     roleName;
+    private String code;
+    private String account;
+    private String phoneNumber;
+    private Long id;
+    private String avatarUrl;
+    
+    
+    
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+
+    
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFullName() {
+        return fullName;
+    }
+    
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    public Long getGender() {
+        return gender;
+    }
+    
+    public void setGender(Long gender) {
+        this.gender = gender;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    
+    public Long getPositionId() {
+        return positionId;
+    }
+    
+    public void setPositionId(Long positionId) {
+        this.positionId = positionId;
+    }
+    
+    public Long getRoleId() {
+        return roleId;
+    }
+    
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+    
+    public String getClassName() {
+        return className;
+    }
+    
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    
+    public String getMajorName() {
+        return majorName;
+    }
+    
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+    
+    public String getDepartmentName() {
+        return departmentName;
+    }
+    
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+    
+    public String getUserCode() {
+        return userCode;
+    }
+    
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getRoleName() {
+        return roleName;
+    }
+    
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+    public String getAccount() {
+        return account;
+    }
+    
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+}
